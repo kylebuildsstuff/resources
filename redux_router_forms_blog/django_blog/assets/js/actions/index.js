ca91edd88277d06eca316b6548c4756e0ca9a40c
KTruong88 <ktruong008@gmail.com>
2016-04-21 23:26:33 -0400

    Add programmatic navigation after successful blog post.

--- redux_router_forms_blog/django_blog/assets/js/actions/index.js
+++ redux_router_forms_blog/django_blog/assets/js/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
-export const CREATE_POSTS = 'CREATE_POSTS';
+export const CREATE_POST = 'CREATE_POSTS';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=123abcdef4123';
--- redux_router_forms_blog/django_blog/assets/js/components/posts_new.js
+++ redux_router_forms_blog/django_blog/assets/js/components/posts_new.js
@@ -1,10 +1,30 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {reduxForm} from 'redux-form';
 // reduxForm is nearly identical to connect() in react-router
+import {Link} from 'react-router';
 
 import {createPost} from '../actions/index';
 
 class PostsNew extends Component {
+  // react router is availible to all components through context property.
+  // to get access to react router, we define contextType. contextType operates
+  // like props but reverse. It searches 'router' in app's context by parsing
+  // through this component's parent components.
+  // Think of it like this: if props is passing data downwards like a waterfall,
+  // context is like a salmon going up the waterfall to retrieve food - food being
+  // the data, 'router', or whatever is inside the contextTypes object.
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  onSubmit(props) { // props from the form .... title, categories...
+    this.props.createPost(props)
+      .then(() => {
+        // blog post has been created, navigate the user to index
+        this.context.router.push('/');
+      });
+  }
+
   render() {
     const {fields: {title, categories, content}, handleSubmit} = this.props;
     // destructuring:
@@ -13,7 +33,7 @@ class PostsNew extends Component {
     // ie. const categories = this.props.fields.categories
 
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create a New Post</h3>
 
         <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
@@ -41,6 +61,7 @@ class PostsNew extends Component {
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
