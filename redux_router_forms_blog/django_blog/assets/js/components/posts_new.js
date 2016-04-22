e9c49fd1d555f07a978b367e4a949798aedb3277
KTruong88 <ktruong008@gmail.com>
2016-04-21 22:50:15 -0400

    Add basic form validation.

--- redux_router_forms_blog/django_blog/assets/js/components/posts_new.js
+++ redux_router_forms_blog/django_blog/assets/js/components/posts_new.js
@@ -16,19 +16,28 @@ class PostsNew extends Component {
       <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create a New Post</h3>
 
-        <div className="form-group">
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title}/>
+          <div className="text-help">
+            {title.touched ? title.error : ''}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
           <label>Categories</label>
           <input type="text" className="form-control" {...categories}/>
+          <div className="text-help">
+            {categories.touched ? categories.error : ''}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
           <label>Content</label>
           <textarea type="text" className="form-control" {...content}/>
+          <div className="text-help">
+            {content.touched ? content.error : ''}
+          </div>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
@@ -37,7 +46,25 @@ class PostsNew extends Component {
   }
 }
 
+// validate gets called with the 'values' from the form
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter a username';
+  };
+  if (!values.categories) {
+    errors.categories = 'Enter a category';
+  };
+  if (!values.content) {
+    errors.content = 'Enter content';
+  };
+
+  return errors;
+}
+
 export default reduxForm({
   form: 'PostsNewForm',  // value doesn't have to match component's name (just has to be unique)
   fields: ['title', 'categories', 'content'],
+  validate
 }, null, {createPost})(PostsNew);
