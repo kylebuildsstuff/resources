b0cb5217b08b8ab4c9b53645fe6eba0f4a303ae6
KTruong88 <ktruong008@gmail.com>
2016-04-23 19:32:04 -0400

    Create serializer and hook up to views/urls.

--- django_REST_framework/hydra/urls.py
+++ django_REST_framework/hydra/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url, include
 from hydra import views
 
 urlpatterns = [
-url(r'^', views.index, name='index'),
+    url(r'^snippets/$', views.snippet_list),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
