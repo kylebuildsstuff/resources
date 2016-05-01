418b80712c182a039510b5f9a8221a4301638077
KTruong88 <ktruong008@gmail.com>
2016-04-30 20:34:29 -0400

    ...Refer to django_rest_framework_2

--- django_REST_framework/hydra/urls.py
+++ django_REST_framework/hydra/urls.py
@@ -1,35 +1,9 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from hydra.views import SnippetViewSet, UserViewSet
+from hydra import views
 from rest_framework import renderers
 
-# <<<<<<< HEAD
-#
-# urlpatterns = format_suffix_patterns([
-#     url(r'^$', views.api_root),
-#     url(r'^snippets/$',
-#         views.SnippetList.as_view(),
-#         name='snippet-list'),
-#     url(r'^snippets/(?P<pk>[0-9]+)/$',
-#         views.SnippetDetail.as_view(),
-#         name='snippet-detail'),
-#     url(r'^snippets/(?P<pk>[0-9]+)/highlight/$',
-#         views.SnippetHighlight.as_view(),
-#         name='snippet-highlight'),
-#     url(r'^users/$',
-#         views.UserList.as_view(),
-#         name='user-list'),
-#     url(r'^users/(?P<pk>[0-9]+)/$',
-#         views.UserDetail.as_view(),
-#         name='user-detail'),
-# ])
-#
-# urlpatterns += [
-#     url(r'^api-auth/', include('rest_framework.urls',
-#                                namespace='rest_framework')),
-# ]
-# =======
 urlpatterns = [
     url(r'^snippets/$', views.SnippetList.as_view()),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
@@ -38,4 +12,3 @@ urlpatterns = [
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
-# >>>>>>> parent of 5e624dd... Add hyperlinking to users and snippets and root api.
--- django_REST_framework/hydra/views.py
+++ django_REST_framework/hydra/views.py
@@ -15,36 +15,12 @@ from hydra.serializers import SnippetSerializer, UserSerializer
 from hydra.permissions import IsOwnerOrReadOnly
 
 
-# @api_view(['GET'])
-# def api_root(request, format=None):
-#     return Response({
-#         'users': reverse('user-list', request=request, format=format),
-#         'snippets': reverse('snippet-list', request=request, format=format),
-#     })
-
-class SnippetViewSet(viewsets.ModelViewSet):
-    """
-    This viewset automatically provides 'list', 'create', 'retrieve',
-    'update' and 'destroy' actions.
-
-    Additionally we also provide an extra 'highlight' action.
-    """
-# from rest_framework.views import APIView
-
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-
-# @NOTE: Needed for part 1 of tutorial
-# from rest_framework.renderers import JSONRenderer
-# from rest_framework.parsers import JSONParser
-
-from django.contrib.auth.models import User
-from rest_framework import status, mixins, generics
-from rest_framework import permissions
-
-from hydra.models import Snippet
-from hydra.serializers import SnippetSerializer, UserSerializer
-from hydra.permisions import IsOwnerOrReadOnly
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format),
+    })
 
 class SnippetList(generics.ListCreateAPIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
@@ -61,45 +37,28 @@ class SnippetList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    This viewset automatically provides 'list' and 'detail' actions.
-    """
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-    # class SnippetHighlight(generics.GenericAPIView):
-    #     queryset = Snippet.objects.all()
-    #     renderer_classes = (renderers.StaticHTMLRenderer,)
-    #
-    #     def get(self, request, *args, **kwargs):
-    #         snippet = self.get_object()
-    #         return Response(snippet.highlighted)
-# # //////////////////////////////////////////////////////////////////
-# class SnippetList(generics.ListCreateAPIView):
-#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-#     queryset = Snippet.objects.all()
-#     serializer_class = SnippetSerializer
-#
-#     def perform_create(self, serializer):
-#         serializer.save(owner=self.request.user)
-#
-# class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-#                           IsOwnerOrReadOnly,)
-#     queryset = Snippet.objects.all()
-#     serializer_class = SnippetSerializer
 
 
-#
-# class UserList(generics.ListAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-#
-# class UserDetail(generics.RetrieveAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
+
+
+
 
 # @NOTE: below is code for second half of part 3 of tutorial, using mixins
 # class SnippetList(mixins.ListModelMixin,
