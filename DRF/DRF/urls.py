1992e4e770927d1c78d88fd452f65efeb4f94b93
KTruong88 <ktruong008@gmail.com>
2016-05-01 16:19:25 -0400

    Add API authorization and permissions.

--- DRF/DRF/urls.py
+++ DRF/DRF/urls.py
@@ -4,4 +4,11 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('snippets.urls')),
+    # url(r'^api-auth/', include('rest_framework.urls',
+    #                            namespace='rest_framework')),
+]
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 ]
--- DRF/snippets/views.py
+++ DRF/snippets/views.py
@@ -1,14 +1,37 @@
-from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+# @NOTE: Using generic CBVs
+from django.contrib.auth.models import User
 from rest_framework import generics
+from rest_framework import permissions
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+
+
+
 
 # @NOTE: Using Mixins with GenericAPIView
 # from snippets.models import Snippet
@@ -103,7 +126,7 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 #
 # from snippets.models import Snippet
 # from snippets.serializers import SnippetSerializer
-
+#
 # class JSONResponse(HttpResponse):
 #     def __init__(self, data, **kwargs):
 #         content = JSONRenderer().render(data)
