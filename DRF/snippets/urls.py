70cf285f84603e44ad4eb3ecf956c5b2a2fb4848
KTruong88 <ktruong008@gmail.com>
2016-05-01 13:02:43 -0400

    Add serializers, models, urls, views using basic tools.

--- DRF/snippets/urls.py
+++ DRF/snippets/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from snippets import views
 
 urlpatterns = [
-    url(r'^', views.index, name='index'),
+    url(r'^snippets/$', views.snippet_list),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
