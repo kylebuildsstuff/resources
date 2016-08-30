0538d7a5fec59494bb4bfa21d502e25f47158701
Kyle Truong <KTruong@cyphersystems.com>
2016-08-30 08:45:24 -0400

    attempt redux form integrate

--- adv_todo/app/components/Todo/index.js
+++ adv_todo/app/components/Todo/index.js
@@ -5,81 +5,108 @@
 */
 
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-// import styles from './styles.css';
-
-class Todo extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      term: '',
-    };
-
-    this.deleteTodo = this.deleteTodo.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
-  }
-
-  handleFormSubmit(event) {
-    event.preventDefault();
-    const data = this.props.todo.set('title', this.state.term);
-    this.props.updateTodo(data);
-    this.setState({
-      term: '',
-    });
-  }
-
-
-  handleInputChange(event) {
-    this.setState({
-      term: event.target.value,
-    });
-  }
-
-  deleteTodo() {
-    this.props.deleteTodo(this.props.todo.get('id'));
-  }
-
-  render() {
-    return (
+const Todo = (props) => {
+  const { handleSubmit, pristine, reset, submitting } = props;
+  return (
+    <form onSubmit={handleSubmit}>
       <div>
-        <div>{this.props.todo.get('number')}</div>
-        <div>{this.props.todo.get('author')}</div>
-        <div>{this.props.todo.get('title')}</div>
-        <form
-          onSubmit={this.handleFormSubmit}
-          className="input-group"
-        >
-          <input
-            placeholder="edit ths thing"
-            className="form-control"
-            value={this.state.term}
-            onChange={this.handleInputChange}
-          />
-          <button
-            type="submit"
-            className="btn btn-secondary"
-          >
-              Submit
-          </button>
-        </form>
-        <div
-          className="btn btn-danger"
-          onClick={this.deleteTodo}
-        >
-          Delete
+        <label htmlFor="firstName">First Name</label>
+        <div>
+          <Field name="firstName" component="input" type="text" placeholder="First Name" />
         </div>
-        <br />
       </div>
-    );
-  }
-}
-
-Todo.propTypes = {
-  updateTodo: React.PropTypes.func,
-  deleteTodo: React.PropTypes.func,
-  todo: React.PropTypes.object,
+      <div>
+        <label htmlFor="lastName">Last Name</label>
+        <div>
+          <Field name="lastName" component="input" type="text" placeholder="Last Name" />
+        </div>
+      </div>
+      <div>
+        <button type="submit" disabled={pristine || submitting}>Submit</button>
+      </div>
+    </form>
+  );
 };
 
-export default Todo;
+export default reduxForm({
+  form: 'simple',
+})(Todo);
+
+
+// class Todo extends React.Component { // eslint-disable-line react/prefer-stateless-function
+//   constructor(props) {
+//     super(props);
+//
+//     this.state = {
+//       term: '',
+//     };
+//
+//     this.deleteTodo = this.deleteTodo.bind(this);
+//     this.handleInputChange = this.handleInputChange.bind(this);
+//     this.handleFormSubmit = this.handleFormSubmit.bind(this);
+//   }
+//
+//   handleFormSubmit(event) {
+//     event.preventDefault();
+//     const data = this.props.todo.set('title', this.state.term);
+//     this.props.updateTodo(data);
+//     this.setState({
+//       term: '',
+//     });
+//   }
+//
+//
+//   handleInputChange(event) {
+//     this.setState({
+//       term: event.target.value,
+//     });
+//   }
+//
+//   deleteTodo() {
+//     this.props.deleteTodo(this.props.todo.get('id'));
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <div>{this.props.todo.get('number')}</div>
+//         <div>{this.props.todo.get('author')}</div>
+//         <div>{this.props.todo.get('title')}</div>
+//         <form
+//           onSubmit={this.handleFormSubmit}
+//           className="input-group"
+//         >
+//           <input
+//             placeholder="edit ths thing"
+//             className="form-control"
+//             value={this.state.term}
+//             onChange={this.handleInputChange}
+//           />
+//           <button
+//             type="submit"
+//             className="btn btn-secondary"
+//           >
+//               Submit
+//           </button>
+//         </form>
+//         <div
+//           className="btn btn-danger"
+//           onClick={this.deleteTodo}
+//         >
+//           Delete
+//         </div>
+//         <br />
+//       </div>
+//     );
+//   }
+// }
+//
+// Todo.propTypes = {
+//   updateTodo: React.PropTypes.func,
+//   deleteTodo: React.PropTypes.func,
+//   todo: React.PropTypes.object,
+// };
+//
+// export default Todo;
