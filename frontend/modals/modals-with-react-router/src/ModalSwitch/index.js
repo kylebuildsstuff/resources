ff25d20f6ca194f8d174a65adf691756013e2a53
Kyle Truong <ktruong008@gmail.com>
2017-06-03 12:37:23 -0400

    - Add notes to the Modal from react-router

--- frontend/modals/modals-with-react-router/src/ModalSwitch/index.js
+++ frontend/modals/modals-with-react-router/src/ModalSwitch/index.js
@@ -9,6 +9,19 @@ import Modal from '../Modal';
 import Gallery from '../Gallery';
 import ImageView from '../ImageView';
 
+// We can pass a location to <Switch/> that will tell it to
+// ignore the router's current location and use the location
+// prop instead.
+//
+// We can also use "location state" to tell the app the user
+// wants to go to `/images/2` in a modal, rather than as the
+// main page, keeping the gallery visible behind it.
+//
+// Normally, `/images/2` wouldn't match the gallery at `/`.
+// So, to get both screens to render, we can save the old
+// location and pass it to Switch, so it will think the location
+// is still `/` even though its `/images/2`.
+
 export class ModalSwitch extends React.Component {
   previousLocation = this.props.location; // ES8 or ES9 stuff... object properties, the same as defining methods on classes like method = () => {...}
 
@@ -24,7 +37,8 @@ export class ModalSwitch extends React.Component {
 
   render() {
     const { location } = this.props;
-    const isModal = !!(
+    const isModal = !!( // Short for isTrue... returns true or false
+      // It converts a nonboolean to an inverted boolean (for instance, !5 would be false, since 5 is a non-false value in JS), then boolean-inverts that so you get the original value as a boolean (so !!5 would be true)
       location.state &&
       location.state.modal &&
       this.previousLocation !== location
