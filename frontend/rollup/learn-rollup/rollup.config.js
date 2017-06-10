69916ed691d7bd196e65b569114b8b785cf3830a
Kyle Truong <ktruong008@gmail.com>
2017-06-09 21:11:39 -0400

    - Add uglifier plugin

--- frontend/rollup/learn-rollup/rollup.config.js
+++ frontend/rollup/learn-rollup/rollup.config.js
@@ -3,7 +3,10 @@ import babel from 'rollup-plugin-babel';
 import eslint from 'rollup-plugin-eslint';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
 
+// Run this in the CLI like this: NODE_ENV=production rollup -c
 export default {
   entry: 'src/scripts/main.js',
   dest: 'build/js/main.min.js',
@@ -25,5 +28,9 @@ export default {
     babel({  // turns ES6 into compatible ES5 in the build/js/main.min.js
       exclude: 'node_modules/**',
     }),
+    replace({
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+    }),
+    (process.env.NODE_ENV === 'production' && uglify()),
   ],
 };
--- frontend/rollup/learn-rollup/src/scripts/main.js
+++ frontend/rollup/learn-rollup/src/scripts/main.js
@@ -8,11 +8,16 @@ import addArray from './modules/mod2';
 import debug from 'debug';
 const log = debug('app:log');
 
-debug.enable('*');
-log('Logging is enabled!');  // because this is a node module, it's commonJS
-// therefore when you bundle it, it treates debug as an external dependency (assumes it will be passed into the global window).
-// so we need some rollup plugins that will load node packages from the node_modules (rollup-plugin-node-resolve)
-// and another module that transpiles commonJS modules to es2015 modules without choking (rollup-plugin-commonjs)
+// Disable logging in production
+if (ENV !== 'production') {
+  debug.enable('*');
+  log('Logging is enabled!');  // because this is a node module, it's commonJS
+  // therefore when you bundle it, it treates debug as an external dependency (assumes it will be passed into the global window).
+  // so we need some rollup plugins that will load node packages from the node_modules (rollup-plugin-node-resolve)
+  // and another module that transpiles commonJS modules to es2015 modules without choking (rollup-plugin-commonjs)
+} else {
+  debug.disable();
+}
 
 // Run some functions from our imported modules.
 const result1 = sayHelloTo('Jason');
