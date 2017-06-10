884c6a0120ed5af205918c7ef61ef79edcb898e7
Kyle Truong <ktruong008@gmail.com>
2017-06-09 20:57:46 -0400

    - Add some plugins for commonJS

--- frontend/rollup/learn-rollup/rollup.config.js
+++ frontend/rollup/learn-rollup/rollup.config.js
@@ -1,5 +1,8 @@
 // Rollup plugins
 import babel from 'rollup-plugin-babel';
+import eslint from 'rollup-plugin-eslint';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   entry: 'src/scripts/main.js',
@@ -7,7 +10,19 @@ export default {
   format: 'iife',
   sourceMap: 'inline',
   plugins: [
-    babel({
+    // 3rd-party libraries may not support es6 modules...
+    resolve({
+      jsnext: true,  // packages that try to ease the transition to es6 modules use this option
+      main: true,  // the resolve will look for the 'main' file, even if it's commonJS
+      browser: true,
+    }),
+    commonjs(),
+    eslint({
+      exclude: [
+        'src/styles/**',  // we don't want eslint to run on our styles
+      ],
+    }),
+    babel({  // turns ES6 into compatible ES5 in the build/js/main.min.js
       exclude: 'node_modules/**',
     }),
   ],
--- frontend/rollup/learn-rollup/src/scripts/main.js
+++ frontend/rollup/learn-rollup/src/scripts/main.js
@@ -4,6 +4,16 @@ import { sayHelloTo } from './modules/mod1';
 // whereas normal bundlers would import both functions in mod1.js for the final build in build/js/main.min.js
 import addArray from './modules/mod2';
 
+// Add a debugger
+import debug from 'debug';
+const log = debug('app:log');
+
+debug.enable('*');
+log('Logging is enabled!');  // because this is a node module, it's commonJS
+// therefore when you bundle it, it treates debug as an external dependency (assumes it will be passed into the global window).
+// so we need some rollup plugins that will load node packages from the node_modules (rollup-plugin-node-resolve)
+// and another module that transpiles commonJS modules to es2015 modules without choking (rollup-plugin-commonjs)
+
 // Run some functions from our imported modules.
 const result1 = sayHelloTo('Jason');
 const result2 = addArray([1, 2, 3, 4]);
