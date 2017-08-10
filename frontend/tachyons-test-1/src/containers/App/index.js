955ffc356e5d4343355d867814d270958bed5902
Kyle Truong <ktruong008@gmail.com>
2017-08-10 10:28:51 -0400

    - Add React Native examples for buttons

---
+++ frontend/tachyons-test-1/src/containers/App/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Body from 'components/Body';
+import Footer from 'components/Footer';
+import Header from 'components/Header';
+import Sidebar from 'components/Sidebar';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Sidebar />
+        <Header />
+        <Body />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
