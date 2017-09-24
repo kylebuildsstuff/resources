6d8ea5ece7cd5929d1c100213a5a1e3ca90d0d11
Kyle Truong <ktruong008@gmail.com>
2017-09-24 14:22:39 -0400

    Remove code for redux-form-v7 (moved to blogs)

--- frontend/forms/redux-form-v7-example/src/reducers.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
-
-export default function createReducer(extraReducerObjects = {}) {
-  return combineReducers({
-    form: formReducer,
-    ...extraReducerObjects
-  });
-}
