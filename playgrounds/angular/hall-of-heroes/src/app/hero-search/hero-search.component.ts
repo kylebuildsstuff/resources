aa95e7af238accf5a580a62a48b50dbd1ef1eb82
Kyle Truong <ktruong008@gmail.com>
2018-06-23 09:43:02 -0400

    no message

--- playgrounds/angular/hall-of-heroes/src/app/hero-search/hero-search.component.ts
+++ playgrounds/angular/hall-of-heroes/src/app/hero-search/hero-search.component.ts
@@ -1,15 +1,30 @@
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-search',
   templateUrl: './hero-search.component.html',
-  styleUrls: ['./hero-search.component.css']
+  styleUrls: ['./hero-search.component.css'],
 })
 export class HeroSearchComponent implements OnInit {
+  constructor(private heroService: HeroService) {}
 
-  constructor() { }
+  heroes$: Observable<Array<Hero>>;
+  private searchTerms = new Subject<string>();
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.heroService.searchHeroes(term)),
+    );
   }
 
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 }
